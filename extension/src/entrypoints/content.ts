b66f41b814b9cebf357b29f224e3c7b3fd6dab2b
Torrenté Florian <lukyrouge4@gmail.com>
2025-08-29 22:20:41 +0200

    Add initial Svelte setup with WXT configuration and example components

---
+++ extension/src/entrypoints/content.ts
@@ -0,0 +1,63 @@
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const INFOMANIAK_TOKEN =
+  "hfCxaNKZMMfdB8R9iFa3qeRWfkiyWwZBFwCg4o47RrbY_GSYQMWHrN4HrzG6zMIlVovlnJuXc3CawecQ";
+
+export default defineContentScript({
+  matches: ["*://*.infomaniak.com/*"],
+  allFrames: true,
+  main(ctx) {
+    ctx.addEventListener(window, "wxt:locationchange", async () => {
+      const {mailBoxId} = await browser.storage.local.get("mailBoxId");
+      if (!mailBoxId) {
+        return;
+      }
+
+      const div = document.querySelector("div.message-item");
+      if (!div) {
+        return;
+      }
+
+      const {context_message_uid, folderThreads} = parseMailPattern(
+        div.classList.value
+      );
+
+      const request = new Request(
+        `https://mail.infomaniak.com/api/mail/${mailBoxId}/folder/${folderThreads[0].folderId}/message/${folderThreads[0].threadId}?prefered_format=plain`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${INFOMANIAK_TOKEN}`,
+          },
+        }
+      );
+      try {
+        const response = await fetch(request);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const {data} = await response.json();
+        console.log("Fetched message data:", JSON.stringify(data.body.value));
+      } catch (error) {
+        console.error("Error fetching message data:", error);
+      }
+    });
+  },
+});
+
+export function parseMailPattern(content: string) {
+  console.log("Parsing mail pattern");
+  const mailPattern = /mail-(\d+)@([a-zA-Z0-9-]+)/g;
+  const context_message_uid = [];
+  const folderThreads = [];
+  let match;
+
+  while ((match = mailPattern.exec(content)) !== null) {
+    const [, threadId, folderId] = match;
+    context_message_uid.push(`${threadId}@${folderId}`);
+    folderThreads.push({threadId, folderId});
+  }
+
+  return {context_message_uid, folderThreads};
+}
---
+++ extension/wxt.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig} from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: "src",
+  modules: ["@wxt-dev/module-svelte"],
+  manifest: {
+    permissions: ["storage", "webRequest"],
+    host_permissions: [
+      "*://*.infomaniak.com/*",
+      "*://mail.infomaniak.com/api/*",
+    ],
+  },
+});
