70483a4c0d1fee759b5efb34077bc522f4470b89
stripet <sebspubemail@gmail.com>
2025-08-30 16:28:17 +0200

    jcrois ca marche mec

--- back/tools/tool.ts
+++ back/tools/tool.ts
@@ -25,44 +25,88 @@ export interface FunctionTool {
     parameters: {
       type: string;
       strict?: boolean;
-      properties: {[key: string]: unknown};
+      properties: { [key: string]: unknown };
       required?: string[];
       additionalProperties: boolean;
     };
   };
 }
 
-export class DummyTool implements GPTTool {
-  name = "dummy_tool";
+const flightToolProperties = {
+  Flight_segments: {
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        title: { type: "string", description: "The title of the flight segment in the format From <departure> to <arrival>"},         // "De Mulhouse à Skopje"
+        fromCode: { type: "string", description: "The full name of the departure airport." },      // "MLH"
+        toCode: { type: "string", description: "The full name of the arrival airport." },        // "SKP"
+        departure: { type: "string", description: "The departure time of the flight in ISO format" },     // "15:20"
+        arrival: { type: "string", description: "The arrival time of the flight in ISO format" },       // "17:30"
+        duration: { type: "string", description: "The duration of the flight in the format 2 h 20 that depends on the timezone of departure and arrival" },      // "2 h 20"
+        airline: { type: "string", description: "The airline operating the flight." },       // "Wizz Air"
+        flightNumber: { type: "string", description: "The flight number." },  // "W64793"
+      },
+      description: "Information about one flight",
+      required: ["items"],
+      additionalProperties: false,
+    },
+  },
+  confirmationCode: { type: "string" },
+  passengers: {
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        lastname: { type: "string" },
+        firstname: { type: "string" },
+      },
+      required: ["items"],
+      additionalProperties: false,
+    },
+    required: ["confirmationCode", "passengers", "Flight_segments"],
+    additionalProperties: false,
+  },
+};
+
+export class FlightTool implements GPTTool {
+  name = "flight_tool";
   systemDescription =
-    "This is a dummy tool. Always call it. It is used for debug !";
+    "Gets information out of a flight ticket confirmation email.";
   getResponseTool(): FunctionTool {
     return {
       type: "function",
       function: {
         name: this.name,
         description:
-          "Virtually does nothing. Still should be called for debug.",
+          "pulls information from a flight ticket confirmation email.",
         parameters: {
           type: "object",
+<<<<<<< Updated upstream
           properties: {},
           required: [],
           // additionalProperties: false,
+=======
+          properties: flightToolProperties,
+          strict: true,
+          required: ["confirmation_number", "passengers", "flights"],
+          additionalProperties: false,
+>>>>>>> Stashed changes
         },
       },
     };
   }
   async handleCall(..._args: any[]): Promise<string> {
-    return "Dummy";
+    return JSON.stringify(_args[0], null, 2);
   }
 }
 
-export const GPT_TOOLS = [new DummyTool()] as GPTTool[];
+export const GPT_TOOLS = [new FlightTool()] as GPTTool[];
 
-export async function call_gpt(messages: {content: string; role: string}[]) {
+export async function call_gpt(messages: { content: string; role: string }[]) {
   const request = new Request(API_URL, {
     method: "POST",
-    headers: {...HEADERS},
+    headers: { ...HEADERS },
     body: JSON.stringify({
       messages,
       model: "qwen3",
@@ -86,7 +130,7 @@ export async function call_gpt(messages: {content: string; role: string}[]) {
           content: string | null;
           tool_calls?: {
             id: string;
-            function: {name: string; arguments: string};
+            function: { name: string; arguments: string };
           }[];
         };
       }>;
@@ -133,7 +177,7 @@ export async function call_gpt(messages: {content: string; role: string}[]) {
 
 async function call_tool(tool_call: {
   id: string;
-  function: {name: string; arguments: string};
+  function: { name: string; arguments: string };
 }) {
   const tool = GPT_TOOLS.find((t) => t.name === tool_call.function.name);
   if (!tool) {
